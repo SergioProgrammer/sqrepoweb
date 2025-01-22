287f489a01c747ff9ae92374fe772868c2899b03
SergioProgrammer <sergiosanpacheco@gmail.com>
2025-01-22 23:16:45 +0000

    vercel 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  
 });
 
