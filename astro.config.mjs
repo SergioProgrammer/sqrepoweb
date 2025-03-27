2e36811adffd4a28988e6d1dc6578ce2663e7f89
Sergio <sergiosanpacheco@gmail.com>
2025-03-27 13:46:20 +0000

    java react import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap'; 
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+
 
 export default defineConfig({
   site: 'https://saraquintana.info',
-  integrations: [tailwind(), sitemap()], 
+  integrations: [react(), tailwind(), sitemap()], 
 });
 
 
