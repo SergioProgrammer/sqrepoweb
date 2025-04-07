b5bf31ddfa0c893d1c728c9d1658c2df7d526696
Sergio <sergiosanpacheco@gmail.com>
2025-04-07 11:58:23 +0100

    commit roll

---
+++ src/components/sections/HeroParallax.tsx
@@ -0,0 +1,121 @@
+import React, { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useSpring,
+  type MotionValue,
+} from "framer-motion";
+
+type Product = {
+  title: string;
+  image: string; // Asegúrate de que la propiedad sea "image"
+};
+
+interface HeroParallaxProps {
+  products: Product[];
+}
+
+const HeroParallax: React.FC<HeroParallaxProps> = ({ products }) => {
+  const firstRow = products.slice(0, 5);
+  const secondRow = products.slice(5, 10);
+  const thirdRow = products.slice(10, 15);
+
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
+
+  const translateX = useSpring(
+    useTransform(scrollYProgress, [0, 1], [0, 1000]),
+    springConfig
+  );
+  const translateXReverse = useSpring(
+    useTransform(scrollYProgress, [0, 1], [0, -1000]),
+    springConfig
+  );
+  const rotateX = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [15, 0]),
+    springConfig
+  );
+  const opacity = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [0.2, 1]),
+    springConfig
+  );
+  const rotateZ = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [20, 0]),
+    springConfig
+  );
+  const translateY = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [-700, 500]),
+    springConfig
+  );
+
+  return (
+    <div
+      ref={ref}
+      className="h-[300vh] py-40 overflow-hidden antialiased relative flex flex-col [perspective:1000px] [transform-style:preserve-3d]"
+    >
+      <Header />
+      <motion.div style={{ rotateX, rotateZ, translateY, opacity }}>
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
+          {firstRow.map((product) => (
+            <ProductCard product={product} translate={translateX} key={product.title} />
+          ))}
+        </motion.div>
+        <motion.div className="flex flex-row mb-20 space-x-20">
+          {secondRow.map((product) => (
+            <ProductCard product={product} translate={translateXReverse} key={product.title} />
+          ))}
+        </motion.div>
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
+          {thirdRow.map((product) => (
+            <ProductCard product={product} translate={translateX} key={product.title} />
+          ))}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+const Header = () => (
+  <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
+    <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
+      Los últimos <br /> desarrollos del studio
+    </h1>
+    <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+    Creamos productos novedosos utilizando las tecnologías y frameworks más recientes. Somos un equipo de desarrolladores y diseñadores apasionados que aman construir cosas increíbles.    </p>
+  </div>
+);
+
+const ProductCard = ({
+  product,
+  translate,
+}: {
+  product: Product;
+  translate: MotionValue<number>;
+}) => (
+  <motion.div
+    style={{ x: translate }}
+    whileHover={{ y: -20 }}
+    className="group/product h-96 w-[30rem] relative shrink-0"
+  >
+    <div className="block group-hover/product:shadow-2xl">
+      <img
+        src={product.image} 
+        alt={product.title}
+        className="object-cover object-left-top absolute h-full w-full inset-0"
+      />
+    </div>
+    <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none" />
+    <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
+      {product.title}
+    </h2>
+  </motion.div>
+);
+
+export default HeroParallax;
